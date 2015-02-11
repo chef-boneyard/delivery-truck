f0167d21926ea72d86ef27396b247bcc2b44b7a2
Tom Duffield <tom@chef.io>
2015-02-11 14:29:04 -0600

    delivery-truck should install chefdk
    This change is a temporary one meant to install a working ChefDK
    in the default recipe. Right now this is a specific version of
    the ChefDK meant to ublock the Engineering team. This should be
    replaced with a more global form of logic.

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,14 @@ ENV['PATH'] = "/opt/chefdk/bin:/opt/chefdk/embedded/bin:#{ENV['PATH']}"
 
 #######################################################################
 
-gem_package "chefspec" do
-  version "4.1.1"
+remote_file "chefdk-0.4.0" do
+  path "#{Chef::Config[:file_cache_path]}/chefdk-0.4.0.deb"
+  source "https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/trusty/chefdk_0.4.0-1_amd64.deb"
+  checksum "e135c0719fc80fc7b95560e90839103167308a45d4927cf8da9c22bdc385cc7d"
+  notifies :install, "dpkg_package[chefdk-0.4.0]", :immediately
+end
+
+dpkg_package "chefdk-0.4.0" do
+  source "#{Chef::Config[:file_cache_path]}/chefdk-0.4.0.deb"
+  action :nothing
 end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe "delivery-truck::default" do
     let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
-    it 'installs chefspec gem' do
-      expect(chef_run).to install_gem_package('chefspec')
-        .with(version: '4.1.1')
+    it 'downloads chefdk package' do
+      expect(chef_run).to create_remote_file('chefdk-0.4.0')
+    end
+
+    it 'installs chefdk package when downloaded' do
+      remote_file = chef_run.remote_file('chefdk-0.4.0')
+      expect(remote_file).to notify('dpkg_package[chefdk-0.4.0]')
+                              .to(:install).immediately
     end
 end
