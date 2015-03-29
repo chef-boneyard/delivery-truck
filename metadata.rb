b5329b7cbb067de821d7ad5645c7461868ec4dee
Seth Falcon <seth@chef.io>
2015-03-29 13:30:49 -0700

    Add environment pinning to provision phase recipe
    Update environment pinning in provision phase and ensure that we pull
    pinnings from Union when in Acceptance.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'delivery-team@chef.io'
 license          'Apache 2.0'
 description      'Delivery build_cookbook for your cookbooks!'
 
-version          '1.0.1'
+version          '1.1.0'
 
 source_url       'https://github.com/opscode-cookbooks/delivery-truck'
 issues_url       'https://github.com/opscode-cookbooks/delivery-truck/issues'
