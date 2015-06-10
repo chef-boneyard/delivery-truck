3268e5ca3fc936e88fae15862d3d09422602a1f4
Salim Afiune <afiune@chef.io>
2015-06-10 18:31:27 -0500

    Remove Berksfile.lock

--- spec/unit/recipes/publish_spec.rb
+++ spec/unit/recipes/publish_spec.rb
@@ -316,7 +316,7 @@ describe "delivery-truck::publish" do
                                     remote_url: 'git@github.com:spec/spec.git',
                                     repo_path: '/tmp/repo',
                                     cache_path: '/tmp/cache',
-                                    action: [:push])
+                                    action: :push)
     end
   end
 
