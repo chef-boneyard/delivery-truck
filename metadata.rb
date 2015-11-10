23d731a9bfdb4446d38355b25a5f796533ab28d2
Seth Chisamore <schisamo@chef.io>
2015-11-09 22:36:03 -0500

    Add workaround for RuboCop 0.35.0 `Etc.getlogin` bug
    If a builder executes the `rubocop` command with the latest version the
    job fails with a `no implicit conversion of nil into String` error. This
    is a known issue with this version and has a fix pending:
    https://github.com/bbatsov/rubocop/issues/2407
    https://github.com/bbatsov/rubocop/pull/2408
    
    The easiest delivery-truck workaround is to ensure $USER is set in the
    subprocess that executes the linting.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'delivery-team@chef.io'
 license          'Apache 2.0'
 description      'Delivery build_cookbook for your cookbooks!'
 
-version          '1.100.7'
+version          '1.100.8'
 
 source_url       'https://github.com/chef-cookbooks/delivery-truck'
 issues_url       'https://github.com/chef-cookbooks/delivery-truck/issues'
