1107e4b2fe8d62ea19e77c702c69201b65303d9a
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2016-03-07 14:01:18 -0500

    bumped_version? checks if cookbook version changed
    Previously, bumped_version? checks that if a cookbook in a change request
    has been modified, then its metadata.(rb|json) has also been modified.
    Unfortunately, this does not actually verify if there has been a
    cookbook version bump and if the bump was sufficient after merging.
    
    This change fixes this by actually parsing out the versions in the
    metadata.rb files. It requires the corresponding change in
    delivery-sugar that provides the API in DeliverySugar::Change to parse
    cookbook versions.

--- spec/unit/libraries/helpers_syntax_spec.rb
+++ spec/unit/libraries/helpers_syntax_spec.rb
@@ -1,66 +1,168 @@
 require 'spec_helper'
 
+module DeliverySugar
+  class Change
+  end
+end
+
 describe DeliveryTruck::Helpers::Syntax do
 
   describe '.bumped_version?' do
-    let(:node) do
-      nodee = Chef::Node.new
-      nodee.default['delivery']['workspace']['repo'] = '/tmp/repo'
-      nodee.default['delivery']['change']['sha'] = '0123456789abcdef'
-      nodee
+    let(:node) { double("node") }
+    let(:workspace) { '/tmp/repo' }
+    let(:pipeline) { 'master' }
+    let(:relative_path) { '.' }
+
+    let(:base_version) { '0.0.1' }
+    let(:base_metadata) { double('metadata', name: 'julia', version: base_version) }
+
+    let(:current_version) { '0.0.1' }
+    let(:current_metadata) { double('metadata', name: 'julia', version: current_version) }
+
+    let(:sugar_change) { double('delivery sugar change',
+                                workspace_repo: workspace,
+                                changed_files: changed_files,
+                                pipeline: pipeline,
+                                merge_sha: merge_sha) }
+
+    before do
+      allow(DeliverySugar::Change).to receive(:new).and_return(sugar_change)
+      allow(sugar_change).to receive(:cookbook_metadata)
+        .with(File.expand_path(relative_path, workspace)).and_return(current_metadata)
     end
 
-    before(:each) do
-      allow(DeliveryTruck::Helpers).to receive(:changed_files).and_return(changed_files)
-      allow(DeliveryTruck::Helpers).to receive(:pre_change_sha).and_return('ABCDEFG')
+    context 'with an unmerged change' do
+      let(:merge_sha) { '' }
+
+      before do
+        allow(sugar_change).to receive(:cookbook_metadata)
+          .with(relative_path, 'origin/master').and_return(base_metadata)
+      end
+
+      context 'when root cookbook was updated' do
+        let(:changed_files) { ['README.md', 'recipes/default.rb', 'metadata.rb'] }
+
+        context 'without version bump' do
+          let(:current_version) { '0.0.1' }
+
+          it 'returns false' do
+            expect(described_class.bumped_version?(workspace, node)).to eql false
+          end
+        end
+
+        context 'with version bump' do
+          let(:current_version) { '0.0.2' }
+
+          it 'returns true' do
+            expect(described_class.bumped_version?(workspace, node)).to eql true 
+          end
+        end
+      end
+
+      context 'when non-cookbook file in root cookbook was updated' do
+        let(:changed_files) { ['README.md'] }
+
+        it 'returns true' do
+          expect(described_class.bumped_version?(workspace, node)).to eql true
+        end
+      end
+
+      context 'when non-cookbook file in cookbooks directory was updated' do
+        let(:changed_files) { ['cookbooks/julia/README.md'] }
+        let(:relative_path) { 'cookbooks/julia' }
+
+        it 'returns true' do
+          expect(described_class.bumped_version?(workspace, node)).to eql true
+        end
+      end
+
+      context 'when cookbook in cookbooks directory was updated' do
+        let(:changed_files) { ['cookbooks/julia/README.md', 'cookbooks/julia/recipes/default.rb', 'cookbooks/julia/metadata.rb'] }
+        let(:relative_path) { 'cookbooks/julia' }
+
+        context 'without version bump' do
+          let(:current_version) { '0.0.1' }
+
+          it 'returns false' do
+            expect(described_class.bumped_version?("#{workspace}/#{relative_path}", node)).to eql false
+          end
+        end
+
+        context 'with version bump' do
+          let(:current_version) { '0.0.2' }
+
+          it 'returns true' do
+            expect(described_class.bumped_version?("#{workspace}/#{relative_path}", node)).to eql true
+          end
+        end
+      end
     end
 
-    # context 'when metadata in root cookbook was updated' do
-    #   let(:changed_files) { ['README.md', 'recipes/default.rb', 'metadata.rb'] }
+    context 'with a merged change' do
+      let(:merge_sha) { 'abcdfakefake' }
 
-    #   it 'returns true' do
-    #     expect(described_class.bumped_version?('/tmp/repo', node)).to eql true
-    #   end
-    # end
+      before do
+        allow(sugar_change).to receive(:cookbook_metadata)
+          .with(relative_path, 'abcdfakefake~1').and_return(base_metadata)
+      end
 
-    # context 'when metadata in root cookbook was not updated' do
-    #   let(:changed_files) { ['README.md', 'recipes/default.rb'] }
+      context 'when root cookbook was updated' do
+        let(:changed_files) { ['README.md', 'recipes/default.rb', 'metadata.rb'] }
 
-    #   it 'returns false' do
-    #     expect(described_class.bumped_version?('/tmp/repo', node)).to eql false
-    #   end
-    # end
+        context 'without version bump' do
+          let(:current_version) { '0.0.1' }
 
-    # context 'when non-cookbook file in root cookbook was updated' do
-    #   let(:changed_files) { ['README.md'] }
+          it 'returns false' do
+            expect(described_class.bumped_version?(workspace, node)).to eql false
+          end
+        end
 
-    #   it 'returns false' do
-    #     expect(described_class.bumped_version?('/tmp/repo', node)).to eql true
-    #   end
-    # end
+        context 'with version bump' do
+          let(:current_version) { '0.0.2' }
 
-    # context 'when metadata for cookbook in cookbooks directory was updated' do
-    #   let(:changed_files) { ['cookbooks/julia/README.md', 'cookbooks/julia/recipes/default.rb', 'cookbooks/julia/metadata.rb'] }
+          it 'returns true' do
+            expect(described_class.bumped_version?(workspace, node)).to eql true 
+          end
+        end
+      end
 
-    #   it 'returns true' do
-    #     expect(described_class.bumped_version?('/tmp/repo/cookbooks/julia', node)).to eql true
-    #   end
-    # end
+      context 'when non-cookbook file in root cookbook was updated' do
+        let(:changed_files) { ['README.md'] }
 
-    # context 'when metadata for cookbook in cookbooks directory was not updated' do
-    #   let(:changed_files) { ['cookbooks/julia/README.md', 'cookbooks/julia/recipes/default.rb' ] }
+        it 'returns true' do
+          expect(described_class.bumped_version?(workspace, node)).to eql true
+        end
+      end
 
-    #   it 'returns false' do
-    #     expect(described_class.bumped_version?('/tmp/repo/cookbooks/julia', node)).to eql false
-    #   end
-    # end
+      context 'when non-cookbook file in cookbooks directory was updated' do
+        let(:changed_files) { ['cookbooks/julia/README.md'] }
+        let(:relative_path) { 'cookbooks/julia' }
 
-    # context 'when non-cookbook file in root cookbook was updated' do
-    #   let(:changed_files) { ['cookbooks/julia/README.md'] }
+        it 'returns true' do
+          expect(described_class.bumped_version?(workspace, node)).to eql true
+        end
+      end
 
-    #   it 'returns false' do
-    #     expect(described_class.bumped_version?('/tmp/repo/cookbooks/julia', node)).to eql true
-    #   end
-    # end
+      context 'when cookbook in cookbooks directory was updated' do
+        let(:changed_files) { ['cookbooks/julia/README.md', 'cookbooks/julia/recipes/default.rb', 'cookbooks/julia/metadata.rb'] }
+        let(:relative_path) { 'cookbooks/julia' }
+
+        context 'without version bump' do
+          let(:current_version) { '0.0.1' }
+
+          it 'returns false' do
+            expect(described_class.bumped_version?("#{workspace}/#{relative_path}", node)).to eql false
+          end
+        end
+
+        context 'with version bump' do
+          let(:current_version) { '0.0.2' }
+
+          it 'returns true' do
+            expect(described_class.bumped_version?("#{workspace}/#{relative_path}", node)).to eql true
+          end
+        end
+      end
+    end
   end
 end
