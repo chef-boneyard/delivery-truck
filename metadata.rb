071227127ed4e49e5ace641f85767c074af8f9d1
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2016-03-11 15:27:09 -0500

    Bump metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'delivery-team@chef.io'
 license          'Apache 2.0'
 description      'Delivery build_cookbook for your cookbooks!'
 
-version          '2.0.5'
+version          '2.0.6'
 
 source_url       'https://github.com/chef-cookbooks/delivery-truck'
 issues_url       'https://github.com/chef-cookbooks/delivery-truck/issues'
@@ -13,4 +13,4 @@ supports 'ubuntu', '>= 12.04'
 supports 'redhat', '>= 6.5'
 supports 'centos', '>= 6.5'
 
-depends 'delivery-sugar'
+depends 'delivery-sugar', '>= 1.0.1'
