6918a0e99ada1264bb6c31bcaa07096428a6fbbd
Tim Smith <tsmith@chef.io>
2017-04-24 23:19:54 -0700

    Use a SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'delivery-truck'
 maintainer       'Chef Delivery Team'
 maintainer_email 'delivery-team@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Delivery build_cookbook for your cookbooks!'
 
 version          '2.3.2'
