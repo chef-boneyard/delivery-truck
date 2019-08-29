848b98e26106d1c7d3b06d0121a20cfee7f4c5b0
Tim Smith <tsmith84@gmail.com>
2019-08-29 13:18:46 -0700

    Release 2.4.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# delivery-truck Cookbook CHANGELOG
+
+This file is used to list changes made in each version of the delivery-truck cookbook.
+
+# 2.4.0 (2019-08-29)
+
+- Update the license string in the metadata.rb to a SPDX compliant license string
+- Add resources to bumped version helper
+- Add retry to push job during deploy phase
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Chef Delivery Team'
 maintainer_email 'delivery-team@chef.io'
 license          'Apache-2.0'
 description      'Delivery build_cookbook for your cookbooks!'
-version          '2.3.2'
+version          '2.4.0'
 
 source_url       'https://github.com/chef-cookbooks/delivery-truck'
 issues_url       'https://github.com/chef-cookbooks/delivery-truck/issues'
